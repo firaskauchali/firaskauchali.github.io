8158cefaf948ed20fa55e7858c5cc95f2e47cb01
Firas Kauchali <firaskauchali@gmail.com>
2018-04-19 17:57:17 -0400

    Under Constructions Placeholder v2
    Currently working on a complete redesign of my site and wanted to
    create a fun placeholder until it’s ready.

---
+++ marquee.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $('.marquee').marquee({
+        speed: 250,
+        duplicated: true,
+        gap: 10,
+        delayBeforeStart: 500
+    });
+    
+});
