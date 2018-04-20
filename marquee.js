b535522d332ab226000454f5cf1c5e35db414b58
Firas Kauchali <firaskauchali@gmail.com>
2018-04-19 23:19:18 -0400

    Fixed errors
    Resolved some lint errors and adjusted marquee scrolling speed for
    devices

--- marquee.js
+++ marquee.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
-    
-    $('.marquee').marquee({
-        speed: 250,
-        duplicated: true,
-        gap: 10,
-        delayBeforeStart: 500
-    });
-    
+/* global $, document, window */
+
+$(document).ready(function () {
+
+	if ($(window).width() < 600) {
+		$('.marquee').marquee({
+			speed: 150,
+			duplicated: true,
+			gap: 10,
+			delayBeforeStart: 500
+		});
+	} else {
+		$('.marquee').marquee({
+			speed: 250,
+			duplicated: true,
+			gap: 10,
+			delayBeforeStart: 500
+		});
+	}
+
 });
--- sketch.js
+++ sketch.js
@@ -3,41 +3,37 @@ var imageIndex = Math.floor(Math.random() * maxImages);
 var images = new Array(maxImages);
 
 var drawImage; // Draws Image after first click
-var renderer; // Fixes canvas size/scroll bar issue
 
 var funk; // Plays alert sound on click
 
 function preload() {
-    for (var i = 0; i < images.length; i ++ ) {
-        images[i] = loadImage("images/underconstruction" + i + ".jpg" );
-    }
-    funk = loadSound("funkalicious.mp3");
+	for (var i = 0; i < images.length; i++) {
+		images[i] = loadImage("images/underconstruction" + i + ".jpg");
+	}
+	funk = loadSound("funkalicious.mp3");
 }
 
 function setup() {
-    renderer = createCanvas(windowWidth, windowHeight);
-    renderer.canvas.style.display = 'block';
-}
-
-function windowResized() {
-    renderer = resizeCanvas(windowWidth, windowHeight);
-    renderer.canvas.style.display = 'block';
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    var r = random(800);
-    if (drawImage) {
-        image(images[imageIndex], mouseX, mouseY);
-    }
+	if (drawImage) {
+		image(images[imageIndex], mouseX, mouseY);
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
 
 function mouseReleased() {
-    drawImage = !drawImage;
-    noLoop(); // Stops constant drawing as mouse moves
+	drawImage = !drawImage;
+	noLoop(); // Stops constant drawing as mouse moves
 }
 
 function mousePressed() {
-    imageIndex = int(random(images.length)); // Picks new image randomly when the mouse is clicked
-    loop(); // Starts loop to draw another single image
-    funk.play();
+	imageIndex = int(random(images.length)); // Picks new image randomly when the mouse is clicked
+	loop(); // Starts loop to draw another single image
+	funk.play();
 }
